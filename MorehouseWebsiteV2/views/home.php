21c0b778e6e290847abf8aa29f9bfa15521c8b97
BezDailey <51421173+BezDailey@users.noreply.github.com>
2020-12-27 14:20:08 -0500

    Add files via upload

---
+++ MorehouseWebsiteV2/views/home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/home.css" />
+    <title>Freshman Class of 2024</title>
+</head>
+<body>
+    <ul>
+        <li><img src="../images/logo.png" alt="freshman 2024 logo" /></li>
+        <li><a href="">The House</a></li>
+        <li><a href="">First Year Resources</a></li>
+        <li><a href="">Creativity Spotlight</a></li>
+        <li><a href="">Dr. Davis' Message Board</a></li>
+    </ul>
+    <div id="img_div">
+        <div id="img_div_words">MOREHOUSE 2024</div>
+    </div>
+    <div id="color_block"></div>
+    <div class="slideshow_container">
+        <h1>Important Dates</h1>
+        <div class="slide">
+            <div class="slide_text">
+                <h1>November 11</h1>
+                <p>Last day of classes</p>
+            </div>
+        </div>
+        <a class="prev" onclick="">&#10094;</a>
+        <a class="next" onclick="">&#10095;</a>
+        <div class="slide_number">
+            1/1
+        </div>
+    </div>
+
+</body>
+</html>
